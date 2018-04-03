1d55306f1f09c4af9aa68e89e203e12284934f88
Jiayi Wang <jjiayu889@gmail.com>
2018-04-03 02:54:06 +0100

    Create Initialization.sh
    Add command to install indicator-multiload and terminator
    Add command to disable screen lock

---
+++ Initialization.sh
@@ -0,0 +1,14 @@
+#Update software repositories and Upgrade System
+sudo apt-get update
+sudo apt-get upgrade
+
+#Install System Status Indicator 
+sudo apt-get install indicator-multiload
+
+#Install Terminator
+sudo apt-get install terminator
+
+#Disable Screen Lock
+gsettings set org.gnome.desktop.session idle-delay 0
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+

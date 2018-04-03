3cec6915b599e12be3dba9a3949b0c9441e25553
Jiayi Wang <jjiayu889@gmail.com>
2018-04-03 17:07:31 +0100

    Update Initialization.sh
    Add auto-response to software installations

--- Initialization.sh
+++ Initialization.sh
@@ -1,15 +1,15 @@
 #Update software repositories and Upgrade System
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
 
 #Install System Status Indicator 
-sudo apt-get install indicator-multiload
+sudo apt-get install indicator-multiload -y
 
 #Install Terminator
-sudo apt-get install terminator
+sudo apt-get install terminator -y
 
 #Install Git
-sudo apt-get install git
+sudo apt-get install git -y
 
 #Disable Screen Lock
 gsettings set org.gnome.desktop.session idle-delay 0
